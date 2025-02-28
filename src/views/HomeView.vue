a191d4f0efaad4f71a4d10b1ff56868ded8b4573
Andre Thomas <drethedevjs@gmail.com>
2025-02-28 13:42:27 -0500

    Added offerings section to homepage.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,10 @@ import HeroSlideShow from "../components/HeroSlideShow.vue";
   <main id="homeApp">
     <div class="flex">
       <div class="w-2/5">
-        <img src="/images/andre/andre-augusta-family-photographer.jpg" />
+        <img
+          src="/images/andre/andre-augusta-family-photographer.jpg"
+          class="object-cover h-full w-full"
+        />
       </div>
       <div class="w-3/5 flex flex-col place-content-center text-center">
         <h2>Family Photographer</h2>
@@ -22,15 +25,81 @@ import HeroSlideShow from "../components/HeroSlideShow.vue";
           those photographs and remember some heart-warming past times.
         </p>
 
-        <button class="about-btn"><RouterLink to="/about">Tell Me More</RouterLink></button>
+        <button class="about-btn ctv-btn"><RouterLink to="/about">Tell Me More</RouterLink></button>
+      </div>
+    </div>
+    <div id="offerings" class="flex">
+      <div class="w-7/12 place-items-center grid">
+        <div class="w-3/4 my-32 mx-auto overflow-hidden relative">
+          <img
+            src="/images/home/augusta-ga-family-photographer-love.jpg"
+            class="object-cover h-full w-full"
+          />
+
+          <div class="absolute inset-0 bg-black opacity-25"></div>
+
+          <div id="lifestyle-overlay" class="absolute inset-0 flex flex-col">
+            <h2 id="lifestyle-overlay-heading">Lifestyle</h2>
+            <p id="lifestyle-overlay-text">
+              Any idea, any location. Bring your best mood and I'll do the rest.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="w-5/12 bg-green-100 overflow-hidden">
+        <div class="relative w-full h-full object-cover">
+          <img
+            src="/images/home/augusta-ga-family-photographer-cold.jpg"
+            class="w-full h-full object-cover"
+          />
+
+          <div class="absolute inset-0 bg-black opacity-25"></div>
+
+          <div id="session-overlay" class="absolute inset-0">
+            <h2 id="session-overlay-heading">Full & Half Sessions</h2>
+            <p id="session-overlay-text">
+              Choose a session that suits your needs. One for a few pictures and one for fuller
+              families.
+            </p>
+            <button id="session-contact-btn" class="ctv-btn">Contact</button>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped>
+#session-contact-btn {
+  @apply mt-16 border-0 shadow-none;
+}
+
+#lifestyle-overlay,
+#session-overlay {
+  @apply pl-16 pt-20;
+}
+
+#lifestyle-overlay-heading,
+#session-overlay-heading {
+  @apply mb-3 font-semibold drop-shadow-lg;
+}
+
+#lifestyle-overlay-text,
+#session-overlay-text {
+  @apply text-xl drop-shadow-lg;
+}
+
+#lifestyle-overlay-heading,
+#lifestyle-overlay-text,
+#session-overlay-heading,
+#session-overlay-text {
+  @apply text-white;
+}
+#offerings {
+  @apply flex w-full;
+}
 .about-btn {
-  @apply btn bg-primary text-white hover:bg-white hover:text-primary hover:border-primary ml-auto mt-20 mr-32 uppercase px-14 py-8 tracking-wider;
+  @apply ml-auto mt-20 mr-32;
 }
 
 h2,

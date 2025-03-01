4d206c31f113286c45ebb839ce516d74c3159df3
Andre Thomas <drethedevjs@gmail.com>
2025-03-01 11:32:33 -0500

    Added about page text.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
+<script lang="ts" setup>
+import AboutPoint from "../components/AboutPoint.vue";
+</script>
 <template>
-  <div id="about-intro">
+  <div id="about-intro" class="mt-10">
     <div class="overflow-hidden h-[680px] relative text-center">
       <img
         src="/images/andre/augusta-ga-family-photography-family-portraits.jpg"
@@ -13,15 +16,50 @@
           and I must say that I have loved every second of it. The best part? Providing people with
           something that they will treasure for years.
         </p>
-        <p class="tracking-widest uppercase">scroll to continue</p>
+        <small class="tracking-widest uppercase">scroll to continue</small>
       </div>
+    </div>
+  </div>
+  <div id="about-body" class="flex p-20">
+    <div class="w-1/2 text-center grid grid-cols-1">
+      <AboutPoint>
+        <template #heading>Why Me?</template>
+        <template #blurb>
+          I have come to find that my success doesn't only come from great photographs, but also
+          with my personal touch. My aim is to make sure that all families are comfortable in front
+          of the camera. I have repeat clients because they enjoy their pictures and they enjoy me.
+          Being on the opposite end of the lens isn't easy for anyone so I make it a point to make
+          sure they are very comfortable and, more importantly, that they have enjoy themselves!
+        </template>
+      </AboutPoint>
+      <AboutPoint>
+        <template #heading>My "Why"</template>
+        <template #blurb>
+          I always make it a point to keep photographing the times my wife and I have together. We
+          are four years into marriage and have a child on the way. I like to document our lives
+          together so that when my children (and, eventually, grandchildren) arrive, I can invite
+          them into our experience with our pictures. So my “why” is simple: to make sure all
+          families have keepsakes for generations to come.
+        </template>
+      </AboutPoint>
+      <AboutPoint>
+        <template #heading>Let's Meet</template>
+        <template #blurb>
+          So go ahead and book now so that you can have your very own family photographer in
+          Augusta, GA, Aiken, SC or in the surrounding counties/cities. Let's meet to take some
+          great portraits so that you can have your moment.
+        </template>
+      </AboutPoint>
+    </div>
+    <div class="w-1/2 px-10 py-20">
+      <img src="/images/andre/augusta-ga-family-photography-family-hw.jpg" class="rounded-lg" />
     </div>
   </div>
 </template>
 
 <style scoped>
 #intro-overlay {
-  @apply absolute inset-0 flex flex-col w-3/5 mx-auto text-white justify-center font-light;
+  @apply absolute inset-0 flex flex-col w-3/5 mx-auto text-white justify-center font-light drop-shadow-lg;
 }
 
 #color-overlay {
@@ -33,6 +71,6 @@ h1 {
 }
 
 p {
-  @apply text-white text-xl mb-16;
+  @apply text-white mb-16;
 }
 </style>

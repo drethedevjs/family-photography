bafdf133a0d396db575782a328343062c63843d3
Andre Thomas <drethedevjs@gmail.com>
2025-08-19 16:15:22 -0400

    Added metadata for SEO optimization.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-// import "./assets/main.css";
-
+import { createHead } from "unhead/client";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
+
+declare global {
+  interface Window {
+    __UNHEAD__: ReturnType<typeof createHead>;
+  }
+}
+
+window.__UNHEAD__ = createHead();
 
 const app = createApp(App);
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
+import pageDescriptions from "@/data/pageDescription";
 import AboutView from "@/views/AboutView.vue";
 import ContactView from "@/views/ContactView.vue";
+import { useHead, useSeoMeta } from "unhead";
 import { createRouter, createWebHistory, type RouteLocationNormalizedGeneric } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import InvestmentView from "../views/InvestmentView.vue";
@@ -35,11 +37,61 @@ const router = createRouter({
   ],
 });
 
-router.afterEach((to: RouteLocationNormalizedGeneric) => {
-  if (to.name && to.name !== "home")
-    document.title = `${to.name.toString().toUpperCase()} | Capture the Vision`;
-  else if (to.name?.toString().toLowerCase() === "home")
-    document.title = `Augusta, GA Family Photographer | Capture the Vision`;
+router.beforeEach((to: RouteLocationNormalizedGeneric) => {
+  const pageData = pageDescriptions.find((pd) => pd.pageName === to.name);
+  useHead(window.__UNHEAD__, {
+    htmlAttrs: { lang: "en-US" },
+    link: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+        sizes: "16x16",
+      },
+      {
+        rel: "me",
+        href: "https://csra-web-services.vercel.app/about",
+      },
+    ],
+  });
+
+  const pageTitle =
+    pageData?.pageName === "home"
+      ? "Augusta, GA Family Photographer"
+      : to.name?.toString().toUpperCase();
+
+  useSeoMeta(window.__UNHEAD__, {
+    title: pageTitle,
+    author: "CSRA Web Dev Services",
+    creator: "Andre Thomas",
+    googleSiteVerification: "DKjTbSA6Q7FyD_ivEr-7oYb9g8ZY7q0unLzUSmhMloU",
+    titleTemplate: "%s | Capture the Vision",
+    description: pageData?.description,
+    robots: "index, follow",
+    charset: "utf-8",
+    ogType: "website",
+    ogTitle: "Augusta, GA Family Photographer",
+    ogDescription:
+      "Learn how to bake delicious, moist cupcakes with our easy-to-follow guide. Featuring tips and tricks for beginners.",
+    twitterTitle: "Augusta, GA Family Photographer",
+    twitterDescription:
+      "Learn how to bake delicious, moist cupcakes with our easy-to-follow guide. Featuring tips and tricks for beginners.",
+
+    // og image
+    ogImage: {
+      url: "/images/families/augusta-ga-family-photographer-forrest.jpg",
+      width: 1200,
+      height: 600,
+      alt: "Family walking among the trees in autumn.",
+      type: "image/png",
+    },
+    twitterImage: {
+      url: "/images/families/augusta-ga-family-photographer-forrest.jpg",
+      width: 1200,
+      height: 600,
+      alt: "Family walking among the trees in autumn.",
+      type: "image/png",
+    },
+  });
 });
 
 export default router;

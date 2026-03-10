97c0c7faa6ed133fe3739f5639e1282099660075
Andre Thomas <drethedevjs@gmail.com>
2026-03-09 21:40:33 -0400

    Updated the slideshow to be the height of the view port.

--- app/components/HeroSlideShow.vue
+++ app/components/HeroSlideShow.vue
@@ -3,9 +3,8 @@ import imageHelper from "@/utils/ImageHelper";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/vue";
 
-const { data: heroSlideImages } = await imageHelper.getCloudinaryImageDataByTag(
-  "slide-show"
-);
+const { data: heroSlideImages } =
+  await imageHelper.getCloudinaryImageDataByTag("slide-show");
 </script>
 
 <template>
@@ -20,15 +19,15 @@ const { data: heroSlideImages } = await imageHelper.getCloudinaryImageDataByTag(
       navigation
       :modules="[Navigation, Pagination]"
     >
-      <SwiperSlide :key="idx" v-for:="(image, idx) in heroSlideImages">
-        <div class="md:h-[600px] flex items-center">
+      <SwiperSlide v-for="(image, idx) in heroSlideImages" :key="idx">
+        <div class="w-full overflow-hidden h-[calc(100vh-218px)]">
           <NuxtImg
             provider="cloudinary"
             placeholder
             quality="80"
             :src="image.fileName"
             :alt="`A family enjoying their time at the park - ${idx + 1}`"
-            className="w-full"
+            class="w-full h-full object-cover"
             format="avif"
           />
         </div>

6ac0f63d04d66de0bceeddeafec221c46ecde947
Andre Thomas <drethedevjs@gmail.com>
2026-04-08 21:32:02 -0400

    Added pinterest verify meta tag.

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,10 @@ useHead({
       name: "og:logo",
       content:
         "https://res.cloudinary.com/ctvphotovideo/image/upload/v1774311424/hf-logo-orange_bkksuz.png"
+    },
+    {
+      name: "p:domain_verify",
+      content: "b5e40e0962d7d97d056466dbdc1f56c1"
     }
   ]
 });
